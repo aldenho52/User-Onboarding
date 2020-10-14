9ec244bf58a18467f0f50e329ea8d83bb3904eaf
Alden Ho <aldenho@Aldens-MacBook-Air.local>
2020-10-14 13:13:49 -0700

    added minimal styling, creating layout for form and functions

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,17 +1,66 @@
+import Axios from 'axios';
 import React, {useState} from 'react';
 import './App.css';
 import Form from './Form'
 
-function App() {
+// Initial States
   const initialFormValues = {
+    username: '',
+    email: '',
+    password: '',
+    terms: false
+  }
+
+  const initialFormErrors = {
+    username: '',
+    email: '',
+    password: '',
+    terms: false  
+  }
+
+  const initialUsers = []
+  const initialDisabled = true;
+
+function App() {
+  // States
+  const [users, setUsers] = useState(initialUsers)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(initialDisabled)
+
+  // Axios Post request
+  const postNewUser = (newUser) => {
+    Axios
+      .post('https://reqres.in/api/users', newUser)
+      .then(res => {
+        console.log(res)
+        setUsers([res.data, ...users])
+        setFormValues(initialFormValues)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  const inputChange = (name, value) => {
 
   }
 
-  const [users, setUsers] = useState([])
+  const formSubmit = () => {
+    const newUser = {
+      
+    }
+  }
 
   return (
     <div className="App">
-      <Form />
+      <Form 
+        values={formValues}
+        errors={formErrors}
+        change={inputChange}
+        submit={formSubmit}
+        disabled={disabled}
+      />
     </div>
   );
 }
--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,10 +1,36 @@
 import React from 'react'
 
-export default function Form () {
+export default function Form (props) {
+    const {values, errors, change, submit, disabled} = props
+
+    const onSubmit = (evt) => {
+        evt.preventDefault()
+        submit()
+    }
+
+    const onChange = (e) => {
+        const {name, type, value, checked} = e.target 
+    }
+
     return (
         <div>
             <h1>Create an Account</h1>
-            
+            <form className='form-container'>
+                <label className='label'>Username
+                    <input type='text' name='username' value={values.username} onChange={onChange}   />
+                </label>
+                <label className='label'>Email
+                    <input type='email' name='email' value={values.email} onChange={onChange}   
+                    />
+                </label>
+                <label className='label'>Password
+                    <input type='text' name='password' value={values.password} onChange={onChange}   />
+                </label>
+                <label className='label'>Terms
+                    <input type='checkbox' name='terms' value={values.terms} onChange={onChange} />
+                </label>
+                <button>Submit</button>
+            </form>
         </div>
     )
 }
